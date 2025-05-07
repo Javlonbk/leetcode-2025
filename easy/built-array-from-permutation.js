03212f7a58f531da382138d5fbbb73ab7dbe35aa
Javlonbk <javlonbeabr41@gmail.com>
2025-05-08 02:55:42 +0500

    feat "Built array from permutation" solution

---
+++ easy/built-array-from-permutation.js
@@ -0,0 +1,14 @@
+function buildArray(nums) {
+    const ans = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        ans[i] = nums[nums[i]];
+    }
+
+    return ans
+};
+
+console.log(buildArray([0,2,1,5,3,4]))
+// [0,1,2,4,5,3]
+
+// O(n)
